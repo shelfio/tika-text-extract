e9da8ae3726d0d447c7a45a5a7f791cc27e7fa82
Vlad Golubev <golubev.vld@gmail.com>
2017-02-24 00:53:10 +0200

    Implement uploading extracted text

--- src/text.js
+++ src/text.js
@@ -1,3 +1,20 @@
-export function extract() {
+import {put} from 'axios';
+import intoStream from 'into-stream';
 
+const URL = 'http://localhost:9998/tika';
+
+/**
+ * Extract text from a document
+ * @param {Buffer|String|Promise} input File to extract text from
+ * @returns {Promise.<String>} Extracted text
+ */
+export function extract(input = '') {
+  const fileStream = intoStream(input);
+  const requestParams = {
+    data: fileStream,
+    headers: {Accept: 'text/plain'},
+    maxContentLength: 100 * 1024 * 1024,
+  };
+
+  return put(URL, requestParams).then(response => response.data);
 }
--- src/text.test.js
+++ src/text.test.js
@@ -1,5 +1,21 @@
 import {extract} from './text';
 
+jest.mock('axios');
+import axios from 'axios';
+axios.put = jest.fn(() => Promise.resolve({data: 'extracted text'}));
+
 it('should expose extract function', () => {
   expect(extract).toBeInstanceOf(Function);
+});
+
+it('should put file stream', async() => {
+  await extract();
+
+  expect(axios.put).toBeCalled();
+});
+
+it('should return extracted text', async() => {
+  const text = await extract();
+
+  expect(text).toBe('extracted text');
 });
