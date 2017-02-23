4a66f373221264995c1aea64ef4dbb9a75d92443
Vlad Golubev <golubev.vld@gmail.com>
2017-02-24 00:26:07 +0200

    Test starting a Tika Server

--- src/server.js
+++ src/server.js
@@ -1,3 +1,14 @@
-export function startServer() {
+import {exec} from 'child_process';
 
+/**
+ * Starts a Tika Server on a default localhost:9998
+ */
+export function startServer(artifactPath) {
+  if (!artifactPath) {
+    throw new Error('Please provide path to Tika Server Artifact');
+  }
+
+  const startCommand = `java -Duser.home=/tmp -jar ${artifactPath}`;
+
+  exec(startCommand);
 }
--- src/server.test.js
+++ src/server.test.js
@@ -1,5 +1,18 @@
 import {startServer} from './server';
 
+jest.mock('child_process');
+import {exec} from 'child_process';
+
 it('should expose startServer function', () => {
   expect(startServer).toBeInstanceOf(Function);
+});
+
+it('should throw if artifact path is not provided', () => {
+  expect(startServer).toThrow('Please provide path to Tika Server Artifact');
+});
+
+it('should call exec to spawn a Tika Server', async() => {
+  await startServer('/tmp/tika.jar');
+
+  expect(exec).toBeCalled();
 });
