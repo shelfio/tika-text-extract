684154979f7a0a5f8f24e6ade6d2c872279024dc
Vlad Artyukh <vlad7art13@gmail.com>
2022-01-19 16:34:52 +0200

    Move basic-usage in example folder

--- examples/basic-usage.js
+++ examples/basic-usage.js
@@ -1,9 +1,9 @@
 const testFile = require('fs').readFileSync('./Readme.md');
-const tte = require('./lib').default;
+const tte = require('../lib').default;
 const options = {
   alignWithJava8: true,
   executableJavaPath: 'java/lib/jvm/jre-1.8.0-openjdk-1.8.0.265.b01-1.lambda2.0.1.x86_64/bin/java',
-  firstVersionOfTika: true,
+  useTikaV1: true,
 };
 
 module.exports.handler = async () => {
