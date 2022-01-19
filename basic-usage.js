6e11e5e5121e508e23ed0d8924d5814cbe3544c9
Vlad Artyukh <vlad7art13@gmail.com>
2022-01-19 12:30:43 +0200

    feat: Update basic-usage

---
+++ basic-usage.js
@@ -0,0 +1,14 @@
+const testFile = require('fs').readFileSync('./Readme.md');
+const tte = require('./lib').default;
+const options = {
+  alignWithJava8: true,
+  executableJavaPath: 'java/lib/jvm/jre-1.8.0-openjdk-1.8.0.265.b01-1.lambda2.0.1.x86_64/bin/java',
+};
+
+module.exports.handler = async () => {
+  return tte
+    .startServer('tika-server.jar', options)
+    .then(() => tte.extract(testFile))
+    .then(console.log)
+    .catch(console.log);
+};
--- examples/basic-usage.js
+++
@@ -1,8 +0,0 @@
-const tte = require('../lib');
-const testFile = require('fs').readFileSync('./README.md');
-
-tte
-  .startServer('/tmp/tika.jar')
-  .then(() => tte.extract(testFile))
-  .then(console.log)
-  .catch(console.log);
